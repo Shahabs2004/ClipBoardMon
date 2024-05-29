67fe30235b9f43f10572fed7c382e8b6a55185c7
Shahabs2004 <Shahabs2004@Gmail.com>
2024-05-29 09:44:29 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ClipBoardMon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.btnOpenLog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 0);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(174, 0);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbLog.Location = new System.Drawing.Point(0, 29);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(800, 421);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "";
+            // 
+            // btnOpenLog
+            // 
+            this.btnOpenLog.Location = new System.Drawing.Point(12, 0);
+            this.btnOpenLog.Name = "btnOpenLog";
+            this.btnOpenLog.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenLog.TabIndex = 3;
+            this.btnOpenLog.Text = "Open Log";
+            this.btnOpenLog.UseVisualStyleBackColor = true;
+            this.btnOpenLog.Click += new System.EventHandler(this.btnOpenLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenLog);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "ClipBoard Monitor and Save";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Button btnOpenLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ClipBoardMon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            /*            InitializeComponent();
+                        File.WriteAllText("log.txt", string.Empty); // create an empty log file
+                        btnOpenLog.Click += btnOpenLog_Click;*/
+            InitializeComponent();
+            string logFilePath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "log.txt");
+            if (!File.Exists(logFilePath))
+            {
+                File.Create(logFilePath).Close(); // create an empty log file if it doesn't exist
+            }
+            //btnOpenLog.Click += btnOpenLog_Click; // subscribe to btnOpenLog Click event
+        }
+
+        private void btnOpenLog_Click(object sender, EventArgs e)
+        {
+            string logFilePath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "log.txt");
+            Process.Start("notepad.exe", logFilePath);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear(); // clear clipboard to prevent duplicate values
+            ClipboardMonitor.ClipboardChanged += ClipboardMonitor_ClipboardChanged; // subscribe to clipboard changed event
+            ClipboardMonitor.Start(); // start monitoring clipboard
+            rtbLog.AppendText("Started Monitoring..");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            ClipboardMonitor.Stop(); // stop monitoring clipboard
+            ClipboardMonitor.ClipboardChanged -= ClipboardMonitor_ClipboardChanged; // unsubscribe from clipboard changed event
+            rtbLog.AppendText("Stoped Monitoring..");
+        }
+
+        private void ClipboardMonitor_ClipboardChanged(object sender, EventArgs e)
+        {
+            Invoke(new MethodInvoker(() =>
+            {
+                rtbLog.AppendText($"Clipboard value changed to: {Clipboard.GetText()}\n");
+            }));
+        }
+        public static class ClipboardMonitor
+        {
+            private static string lastClipboardValue = string.Empty;
+
+            public static event EventHandler ClipboardChanged;
+
+            public static void Start()
+            {
+                lastClipboardValue = string.Empty;
+                Timer timer = new Timer();
+                timer.Interval = 100;
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+
+            public static void Stop()
+            {
+                lastClipboardValue = string.Empty;
+            }
+
+            private static void LogClipboardValue(string clipboardValue)
+            {
+                string logFilePath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "log.txt");
+                using (StreamWriter sw = new StreamWriter(logFilePath, true))
+                {
+                    sw.WriteLine($"{DateTime.Now.ToString()}: {clipboardValue}");
+                }
+            }
+
+            private static void Timer_Tick(object sender, EventArgs e)
+            {
+                if (Clipboard.ContainsText())
+                {
+                    string clipboardValue = Clipboard.GetText();
+                    if (clipboardValue != lastClipboardValue)
+                    {
+                        lastClipboardValue = clipboardValue;
+                        LogClipboardValue(clipboardValue);
+                        ClipboardChanged?.Invoke(null, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+    }
+}
